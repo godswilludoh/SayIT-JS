eac63282c36e969e86ecff8416300d5d3b8cfc65
Godswill Udoh <godswilludoh@gmail.com>
2022-09-22 19:52:40 +0100

    working on search functionality

--- js/searchFunct.js
+++ js/searchFunct.js
@@ -1,50 +1,17 @@
-/*searching through report table*/
-const searchInput = document.getElementById('search-input');
-const rows = document.querySelectorAll('.row');
+let searchInput, filter, table, tr;
 
-// searchInput.addEventListener('keyup', searchFunc);
+searchInput = document.getElementById('search-input');
+filter = searchInput.value.toLowerCase();
+table = document.getElementById('user-table');
+tr = table.getElementsByTagName('tr');
 
-// function searchFunc(e) {
-// 	const dataDetails = e.target.value;
+searchInput.addEventListener('keyup', searchFunction);
 
-// 	for (let i = 0; i < rows.length; i++) {
-// 		rows[i].querySelector('td').textContent.includes(dataDetails)
-// 			? (rows.style.display = 'table-row')
-// 			: (rows.style.display = 'none');
-// 	}
-// }
-
-/*adding event listener*/
-searchInput.addEventListener('keyup', (e) => {
-	//convert to lowerCase
-	const textDetails = e.target.value.toLowerCase();
-
-	//get tds
-	let rowsTd = document.querySelectorAll('td');
-	console.log(rowsTd);
-	let tableData =
-		/* Looping through the table rows */
-		rows.forEach((row) => {
-			row.querySelector('td').innerText.match(textDetails)
-				? (row.style.display = 'table-row')
-				: (row.style.display = 'none');
-		});
-});
-
-// filter.addEventListener('keyup', (e) => {
-// 	//convert to lowerCase
-// 	let text = e.target.value.toLowerCase();
-
-// 	//get lis
-// 	let items = itemList.getElementsByTagName('li');
-
-// 	//convert to an array
-// 	Array.from(items).forEach(function (item) {
-// 		let itemName = item.firstChild.textContent;
-// 		if (itemName.toLowerCase().indexOf(text) != -1) {
-// 			item.style.display = 'block';
-// 		} else {
-// 			item.style.display = 'none';
-// 		}
-// 	});
-// });
+function searchFunction() {
+	for (let i = 0; i < tr.length; i++) {
+		let td = tr[i].getElementsByTagName('td')[2];
+		console.log(td.innerText);
+		if (td.innerText.includes) {
+		}
+	}
+}

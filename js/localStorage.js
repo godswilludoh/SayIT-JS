ac9225f0c7daf582629b79e8804f61a996f585ad
Godswill Udoh <godswilludoh@gmail.com>
2022-09-25 02:03:30 +0100

    worked changes

--- js/localStorage.js
+++ js/localStorage.js
@@ -34,26 +34,75 @@
 // // };
 
 const reportForm = (e) => {
-	let reportData = {
+	let reportData = JSON.parse(localStorage.getItem('reportData')) || [];
+	let reports = {
 		sectorDetails: document.getElementById('sector').value,
 		agencyDetails: document.getElementById('agency').value,
 		reportDetails: document.getElementById('report-org').value,
 		reportInfoDetails: document.getElementById('report-info').value,
 		affiliationDetails: document.getElementById('affiliation').value,
 		uploadDetails: document.getElementById('upload').value,
+		dateReported: dateReported(),
+		reportID: Math.floor(Math.random() * Date.now()),
 	};
+
+	reportData.push(reports);
 	localStorage.setItem('reportData', JSON.stringify(reportData));
-	console.log(localStorage.getItem('reportData'));
-	e.preventDefault();
+
 	sectorOutput();
+	e.preventDefault();
 };
 
-function sectorOutput() {
-	let { sectorDetails } = JSON.parse(localStorage.getItem('reportData'));
-	console.log(sectorDetails);
-	let sectorData = document.getElementsByClassName('sector');
-	sectorData.innerHTML = `<p>${sectorDetails}</p>`;
-	const para = document.createElement('p');
-	para.innerHTML = 'This is a paragraph';
-	sectorData.appendChild(para);
+function userTable() {
+	let table = document.querySelector('table');
+	let headerRow = document.createElement('tr');
+	headerRow.innerHTML = `<th>S/N</th>    
+    <th>Date reported</th>
+    <th>Report ID</th>
+    <th>Sector</th>
+    <th>Action</th>`;
+	table.appendChild(headerRow);
+	//getting the table details from local storage
+	let { dateReported, reportID, sectorDetails } = JSON.parse(
+		localStorage.getItem('reportData')
+	);
+	//console.log(dateReported, reportID, sectorDetails);
+	//getting the whole report data details
+	let reportData = JSON.parse(localStorage.getItem('reportData')) || [];
+	console.log(reportData);
+	let i = 1;
+
+	reportData.forEach(function (report) {
+		let newRow = document.createElement('tr');
+		newRow.innerHTML = `<td>${i++}</td> 
+        <td>${report.dateReported}</td> 
+        <td>${report.reportID}</td>       
+        <td>${report.sectorDetails}</td> 
+        <td>
+                <div class="table-icons">
+                  <div>
+                    <a class="report-button view" href="#">View</a>
+                  </div>
+                  <div>
+                    <box-icon name='printer' type='solid'></box-icon>
+                  </div>
+                </div>
+              </td>`;
+		table.appendChild(newRow);
+	});
+}
+
+function dateReported() {
+	let currentdate = new Date();
+	return (
+		currentdate.getDate() +
+		'/' +
+		currentdate.getMonth() +
+		'/' +
+		currentdate.getFullYear() +
+		' ' +
+		currentdate.getHours() +
+		':' +
+		currentdate.getMinutes()
+	);
 }

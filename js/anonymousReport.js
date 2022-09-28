0e12ca42a62ba3f9f3d1b468e473a02b8d616267
Godswill Udoh <godswilludoh@gmail.com>
2022-09-28 08:12:32 +0100

    worked on prompt modal

--- js/anonymousReport.js
+++ js/anonymousReport.js
@@ -1,63 +1,66 @@
 // Get the modal
-let modal = document.getElementById("myModal");
+let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+let btn = document.getElementById('myBtn');
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
-    var sector=document.getElementById('formGroupSector').value;
-    var agency=document.getElementById('formGroupAgency').value;
-    var organization=document.getElementById('reportOrg').value;
-    var affiliation=document.getElementById('yesOrNo').value;
-    var upload=document.getElementById('upload').value;
-    var report=document.getElementById('reportInfo').value;
+	var sector = document.getElementById('formGroupSector').value;
+	var agency = document.getElementById('formGroupAgency').value;
+	var organization = document.getElementById('reportOrg').value;
+	var affiliation = document.getElementById('yesOrNo').value;
+	var upload = document.getElementById('upload').value;
+	var report = document.getElementById('reportInfo').value;
 
-    var checkbox=document.getElementById('Affirm').checked?document.getElementById('Affirm').value:'';
+	var checkbox = document.getElementById('Affirm').checked
+		? document.getElementById('Affirm').value
+		: '';
 
-    console.log(sector, agency, organization, affiliation, upload, report, checkbox)
-   
-    let anonymousReport={
-      sector:sector,
-      agency:agency,
-      organization:organization,
-      affiliation:affiliation,
-      upload:upload,
-      report:report,
-      checkbox:checkbox,
-    }
+	console.log(
+		sector,
+		agency,
+		organization,
+		affiliation,
+		upload,
+		report,
+		checkbox
+	);
 
-    console.log(anonymousReport);
-    localStorage.setItem('anonymousReport_data',JSON.stringify(anonymousReport))
+	let anonymousReport = {
+		sector: sector,
+		agency: agency,
+		organization: organization,
+		affiliation: affiliation,
+		upload: upload,
+		report: report,
+		checkbox: checkbox,
+	};
 
-    modal.style.display = "block";
+	console.log(anonymousReport);
+	localStorage.setItem('anonymousReport_data', JSON.stringify(anonymousReport));
+
+	modal.style.display = 'block';
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+	modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
 };
 
 // retnHome.onclick = function(){
 //   window.location.href = 'https://www.figma.com/file/w44lgvHGdF2YveGTUyyq5o/SayIt-Platform?node-id=4%3A4';
 // }
 
-
-// LOCAL STORAGE STARTS HERE 
-function submit()
-  {
-    
-  }
-  
-
-
+// LOCAL STORAGE STARTS HERE
+function submit() {}
--- js/searchBlog.js
+++ js/searchBlog.js
@@ -1,21 +1,38 @@
-//Add a search function to the DOM
-function searchBlogsBySector() {
-	const input = document.getElementById('search');
-	const blogGridContainer = document.getElementById('blog_grid-container');
-	console.log(blogGridContainer);
+// //Add a search function to the DOM
+// function searchBlogsBySector() {
+// 	const input = document.getElementById('search');
+// 	const blogGridContainer = document.getElementById('blog_grid-container');
+// 	console.log(blogGridContainer);
 
-	const inputValue = input.value.toUpperCase();
-	const blogList = blogGridContainer.querySelectorAll('.blog_box-wrapper'); //returns an array of the blog_box-wrapper class
-	// console.log(blogList)
-	//Looping through all the blogs and rendering the ones that matches the search input
-	for (i = 0; i < blogList.length; i++) {
-		const blogListDivTAg = blogList[i].querySelector('.blog_sector-category');
-		const blogListDivTagValue = blogListDivTAg.innerText.toUpperCase();
-		isMatch = blogListDivTagValue.indexOf(inputValue) > -1;
-		if (isMatch) {
-			blogList[i].style.display = 'block';
-		} else {
-			blogList[i].style.display = 'none';
-		}
-	}
-}
+// 	const inputValue = input.value.toUpperCase();
+// 	const blogList = blogGridContainer.querySelectorAll('.blog_box-wrapper'); //returns an array of the blog_box-wrapper class
+// 	// console.log(blogList)
+// 	//Looping through all the blogs and rendering the ones that matches the search input
+// 	for (i = 0; i < blogList.length; i++) {
+// 		const blogListDivTAg = blogList[i].querySelector('.blog_sector-category');
+// 		const blogListDivTagValue = blogListDivTAg.innerText.toUpperCase();
+// 		isMatch = blogListDivTagValue.indexOf(inputValue) > -1;
+// 		if (isMatch) {
+// 			blogList[i].style.display = 'block';
+// 		} else {
+// 			blogList[i].style.display = 'none';
+// 		}
+// 	}
+// }
+
+const searchInput = document.getElementById('search-input');
+const rows = document.querySelectorAll('.row');
+//console.log(rows);
+
+/*adding event listener*/
+searchInput.addEventListener('keyup', (e) => {
+	const textDetails = e.target.value;
+	//console.log(textDetails);
+
+	/* Looping through the table rows */
+	rows.forEach((row) => {
+		row.querySelector('td').textContent.toLowerCase().startsWith(textDetails)
+			? (row.style.display = 'table-row')
+			: (row.style.display = 'none');
+	});
+});

1423811802f3c6a210c9ebdb29817cca3a927823
Adenigba <lameda789@gmail.com>
2022-10-01 19:55:12 +0100

    agent table now dynamic

--- js/agents.js
+++ js/agents.js
@@ -1,24 +1,26 @@
 const form = document.getElementById("agent-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-// getting agents inputed value from the form
-const agentID = document.getElementById("agentID").value;
-const agentpassword = document.getElementById("agentpassword").value;
-// NOW CHECKING IF THE LOGIN FUNCTION CORELATES WITH THAT OF THE DATABASE TO ALLOW AGENT LOGIN
+  // getting agents inputed value from the form
+  const agentID = document.getElementById("agentID").value;
+  const agentpassword = document.getElementById("agentpassword").value;
+  // NOW CHECKING IF THE LOGIN FUNCTION CORELATES WITH THAT OF THE DATABASE TO ALLOW AGENT LOGIN
 
-// 1) Getting the hardcoded registered database so that i can compare both inputs
-let agentsData = JSON.parse(localStorage.getItem("AgentDatabase"));
+  // 1) Getting the hardcoded registered database so that i can compare both inputs
+  let agentsData = JSON.parse(localStorage.getItem("AgentDatabase"));
 
-// 2) Checking and comapring the input with the database
-let checkAgent = agentsData.some(info => info.Agent_ID == agentID && info.Agent_Login_Password == agentpassword)
-// alert(JSON.stringify(checkAgent));
+  // 2) Checking and comapring the input with the database
+  let checkAgent = agentsData.some(
+    (info) =>
+      info.Agent_ID == agentID && info.Agent_Login_Password == agentpassword
+  );
+  // alert(JSON.stringify(checkAgent));
 
-if(!checkAgent){
-	alert("Invalid login");
-}
-else{
-	window.location.href = "agentdashboard.html"
-}
+  if (!checkAgent) {
+    alert("Invalid login");
+  } else {
+    window.location.href = "agentdashboard.html";
+  }
 });
 
 //local storage containing list of registered agents
@@ -51,7 +53,3 @@ let currentlyRegisteredAgent = [
   },
 ];
 localStorage.setItem("AgentDatabase", JSON.stringify(currentlyRegisteredAgent));
-
-
-
-
--- js/loggedInAgent.js
+++ js/loggedInAgent.js
@@ -1,42 +1,69 @@
+
 // FOR DYNAMICALLY DISPLAYING CURRENT LOGGED IN AGENT
 
 //local storage containing list of registered agents
-let currentlyRegisteredAgent = [
-    {
-      Agency_Name: "Lagos State Vehicle Inspection Officers",
-      Agency_Address: "gowan estate, VI, Lagos State",
-      Agency_Customer_Care_Number: "08054546213",
-      Agent_Name: "Funmi Bankole",
-      Agent_Mail: "Bankole@gmail.com",
-      Preferred_Username: "Bankole",
-      Agent_Phone_Number: "080600012478",
-      Gender: "female",
-      Agent_ID: "#VIOFunmiBankole",
-      Agent_Login_Password: "vioagency",
-    },
-  
-    {
-      Agency_Name: "Nigeria Police Force",
-      Agency_Address: "jonathan estate, VI, Lagos State",
-      Agency_Customer_Care_Number: "08054546213",
-      Agent_Name: "Comrade Bayo",
-      Agent_Mail: "comradebayo@gmail.com",
-      Preferred_Username: "Senior Comrade",
-      Agent_Phone_Number: "08033012478",
-      Gender: "male",
-      Agent_ID: "#NPFComradeBayo",
-      Agent_Login_Password: "nigerianpoliceforce",
-    },
-  ];
+let currentlyRegisteredAgent = JSON.parse(localStorage.getItem("AgentDatabase"));
+
 
 // FOR GETTING AND DISPLAYING THE CURRENTLY LOGGED IN AGENT NAME
 const currrentLoggedIn = document.querySelector(".forTheAgentID");
 currrentLoggedIn.textContent = currentlyRegisteredAgent[0].Agent_Name;
-
 
 // FOR GETTING AND DISPLAYING THE CURRENTLY LOGGED IN AGENT COMPANY NAME
 const currrentLoggedInAgency = document.querySelector(".registeredAgencyName");
 currrentLoggedInAgency.textContent = currentlyRegisteredAgent[0].Agency_Name;
 // console.log(currrentLoggedIn);
 
+// FOR THE AGENTS TABLE DYNAMISM
+let reportData = JSON.parse(localStorage.getItem("reportData"));
 
+function agentsTable() {
+  let table = document.querySelector("table");
+  let headerRow = document.createElement("tr");
+  headerRow.innerHTML = `<th>S/N</th>    
+    <th>Date reported</th>
+    <th>Report ID</th>
+    <th>View Report</th>
+    <th>Status</th>
+    <th>Action</th>`;
+  table.appendChild(headerRow);
+
+  reportData = reportData.filter(
+    (info) =>
+      info.sectorDetails == "VIO" ||
+      info.sectorDetails == "transportation" ||
+      info.sectorDetails == "vehicle inspection officer" ||
+      info.reportDetails == "VIO"
+  );
+
+  //incrementing cards
+  let card = document.querySelector(".theMetricItself");
+  let card2 = document.querySelector(".openComplaint");
+  card.innerHTML = `${reportData.length}`;
+  card2.innerHTML = `${reportData.length}`;
+  
+ 
+  
+  let i = 1;
+  // WRITING INTO THE TABLE
+  reportData.forEach(function (report) {
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = `<td>${i++}</td> 
+        <td>${report.dateReported}</td> 
+        <td>${report.reportID}</td> 
+        <td><a href="#" class="toViewMoreLinke">Click to view</a>    
+        </td>
+        <td>
+              <select>
+                <option value="select" selected>Select</option>
+                <option value="view">In View</option>
+                <option value="completed">Completed</option>
+              </select>
+        </td>
+        <td><div class="forTheUpdateAndDownloadButton">
+              <a href="#" class="forTheDeleteButton">Delete</a><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1)"><path d="M19 7h-1V2H6v5H5a3 3 0 0 0-3 3v7a2 2 0 0 0 2 2h2v3h12v-3h2a2 2 0 0 0 2-2v-7a3 3 0 0 0-3-3zM8 4h8v3H8V4zm0 16v-4h8v4H8zm11-8h-4v-2h4v2z"></path></svg></a>
+            </div>
+        </td>`;
+    table.appendChild(newRow);
+  });
+}
